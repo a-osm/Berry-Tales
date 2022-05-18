eec41761919fef1c5c9449d6485fb7d87f28bff5
a-osm <aosmn97@gmail.com>
2022-05-18 12:03:22 -0400

    initial react setup

--- src/main/frontend/client/components/App.js
+++ src/main/frontend/client/components/App.js
@@ -1,14 +1,26 @@
-import React, { useEffect } from "react";
-import { hot } from "react-hot-loader/root";
-import "foundation-sites";
-import $ from "jquery";
+import React, { useEffect } from "react"
+import { hot } from "react-hot-loader/root"
+import "foundation-sites"
+import $ from "jquery"
+import { Route, BrowserRouter, Switch, Redirect } from "react-router-dom"
 
-const App = (props) => {
+import BerriesIndex from "./BerriesIndex"
+
+const App = props => {
   useEffect(() => {
-    $(document).foundation();
-  }, []);
+    $(document).foundation()
+  }, [])
 
-  return <h1>Hello from the frontend!</h1>;
-};
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/berries" />
+        </Route>
+        <Route exact path="/berries" component={BerriesIndex} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
 
-export default hot(App);
+export default hot(App)
