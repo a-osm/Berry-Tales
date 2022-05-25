bc112c28f70aeb344d736c8ada6d7c2fba86db20
LKou <LCoop19@users.noreply.github.com>
2022-05-25 16:58:53 -0400

    moved css from target folder to client folder

--- src/main/frontend/client/components/BerryTile.js
+++ src/main/frontend/client/components/BerryTile.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import DeleteBerryButton from "./DeleteBerryButton.js"
 
 const BerryTile = props => {
   const { name, imgUrl, description, id } = props.berry
